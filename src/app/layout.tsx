5061335b6dbd5bdb218607c09e516c6777def817
Asteriuz <gutocebola@gmail.com>
2023-11-10 05:02:51 -0300

    Added Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./styles/animation.css";
 import Footer from "@/components/Footer/Footer";
 import Script from "next/script";
 import Header from "@/components/Header/Header";
+import Head from "next/head";
 
 const roboto = Roboto({
   weight: "400",
@@ -25,6 +26,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#2d89ef" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className={roboto.className}>
         <Header />
         <main className="overflow-x-hidden">{children}</main>
