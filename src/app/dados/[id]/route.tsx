e3b266c5b5aaf78b5499b4676f3475ca41f2dd2b
Asteriuz <gutocebola@gmail.com>
2023-11-10 16:52:23 -0300

    Changed Api

--- src/app/dados/[id]/route.tsx
+++ src/app/dados/[id]/route.tsx
@@ -2,10 +2,7 @@ import { NextResponse } from "next/server";
 
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function GET(request: Request, { params }: { params: any }) {
   const id = params.id;
   const res = await fetch(`http://127.0.0.1:8080/${id}`);
   const data: Dados = await res.json();
