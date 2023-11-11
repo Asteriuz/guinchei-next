8bb8159d03e99ff8679cd88f0861663380d1fe63
Asteriuz <gutocebola@gmail.com>
2023-11-11 11:46:06 -0300

    Created a try block for dados

--- src/app/dados/[id]/route.tsx
+++ src/app/dados/[id]/route.tsx
@@ -2,10 +2,14 @@ import { NextResponse } from "next/server";
 
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; caso for usar https
 
-export async function GET() {
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id;
   try {
-    const res = await fetch("http://http://127.0.0.1:8080");
-    const data: Dados[] = await res.json();
+    const res = await fetch(`http://127.0.0.1:8080/${id}`);
+    const data: Dados = await res.json();
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json([]);
